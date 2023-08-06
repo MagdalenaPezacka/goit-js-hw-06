143f0b3c50cc8aaf71a411584971acca2252a292
MagdalenaPezacka <magdalena.pezacka.it@gmail.com>
2023-08-06 21:50:35 +0200

    task1

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,11 @@
-const numberOfCategories = categories.length;
-console.log(numberOfCategories);
+console.log(categories)
+const itemElements = document.querySelectorAll("li.item");
+console.log(`Number of categories:${itemElements.length}`)
+
+
+itemElements.forEach(item => {
+  const title = item.querySelector("h2");
+  console.log(`Category: ${title.textContent}`)
+const element = item.querySelectorAll("li");
+  console.log(`Elements: ${element.length}`)
+});
